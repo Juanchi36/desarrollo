608907e823071d470edbb4305fcddb19bfa54cf9
gustavo <gustavodevaler@gmail.com>
2019-01-19 00:15:42 -0300

    mejorando contacto.php

--- contacto.php
+++ contacto.php
@@ -4,20 +4,56 @@
 	<title>Contacto</title>
 </head>
 <body>
-	<h2>Tu mensaje ha sido enviado con exito !!</h2>
-
-	<?php
-	$nombre=$_REQUEST['nombre'];
+<?php
+    $nombre=$_REQUEST['nombre'];
 	$email=$_REQUEST['email'];
 	$mensaje=$_REQUEST['mensaje'];
 
-	echo $nombre.'<br>';
-	echo $email.'<br>';
-	echo $mensaje;
+
+require_once('PHPMailer_5.2.4/class.PHPMailer.php'); 
+
+$mail = new PHPMailer(true); 
+$mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
+//$mail->SMTPDebug = 2; 
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'tls';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;// TCP port to connect to
+$mail->CharSet = 'UTF-8';
+$mail->Username ='gustavodevaler@gmail.com'; //Email para enviar
+$mail->Password = 'daiana26?'; //Su password
+//Agregar destinatario
+$mail->setFrom($email,$nombre);
+$mail->AddAddress('gustavodevaler@gmail.com');//A quien mandar email
+$mail->SMTPKeepAlive = true;  
+$mail->Mailer = "smtp"; 
+
+
+    //Content
+$mail->isHTML(true); // Set email format to HTML
+
+
+$mail->Subject = 'Desde tu web';
+$mail->Body    = $mensaje;
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+  echo 'Error al enviar email';
+  echo 'Mailer error: ' . $mail->ErrorInfo;
+} else {
+  echo 'Mail enviado correctamente';
+}
+?>
 
 
 
-	?>
 
 </body>
 </html>
